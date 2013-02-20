645330feab6425afd06b8d7050eab491a5f904c0
Aleksander Hansen <aleksbreian@gmail.com>
2013-02-19 22:12:13 -0600

    Version 5 Valuation

---
+++ our-boxen-master/config/boxen.rb
@@ -0,0 +1,19 @@
+# This file will be loaded by config/basic early in a Boxen run. Use
+# it to provide any custom code or behavior your Boxen setup requires.
+
+# Change the prefix boxen is installed to.
+# ENV['BOXEN_HOME'] = '/opt/boxen'
+
+# Change the repo boxen will use.
+# ENV['BOXEN_REPO_NAME'] = 'boxen/our-boxen'
+
+# Boxen binary packaging
+# ENV["BOXEN_S3_ACCESS_KEY"] = ''
+# ENV["BOXEN_S3_SECRET_KEY"] = ''
+# ENV["BOXEN_S3_BUCKET"] = ''
+
+# Auto-report issues on failed runs
+# ENV["BOXEN_ISSUES_ENABLED"] = 'yes'
+
+# Submit audit data to an arbitrary HTTP endpoint
+# ENV["BOXEN_WEB_HOOK_URL"] = 'https://some-uri.com/boxen'
---
+++ our-boxen-master/manifests/site.pp
@@ -0,0 +1,88 @@
+require boxen::environment
+require homebrew
+require gcc
+
+Exec {
+  group       => 'staff',
+  logoutput   => on_failure,
+  user        => $luser,
+
+  path => [
+    "${boxen::config::home}/rbenv/shims",
+    "${boxen::config::home}/rbenv/bin",
+    "${boxen::config::home}/rbenv/plugins/ruby-build/bin",
+    "${boxen::config::home}/homebrew/bin",
+    '/usr/bin',
+    '/bin',
+    '/usr/sbin',
+    '/sbin'
+  ],
+
+  environment => [
+    "HOMEBREW_CACHE=${homebrew::config::cachedir}",
+    "HOME=/Users/${::luser}"
+  ]
+}
+
+File {
+  group => 'staff',
+  owner => $luser
+}
+
+Package {
+  provider => homebrew,
+  require  => Class['homebrew']
+}
+
+Repository {
+  provider => git,
+  extra    => [
+    '--recurse-submodules'
+  ],
+  require  => Class['git']
+}
+
+Service {
+  provider => ghlaunchd
+}
+
+Homebrew::Formula <| |> -> Package <| |>
+
+node default {
+  # core modules, needed for most things
+  include dnsmasq
+  include git
+  include hub
+  include nginx
+  include nvm
+  include ruby
+
+  # fail if FDE is not enabled
+  if $::root_encrypted == 'no' {
+    fail('Please enable full disk encryption and try again')
+  }
+
+  # node versions
+  include nodejs::0-4
+  include nodejs::0-6
+  include nodejs::0-8
+
+  # default ruby versions
+  include ruby::1-8-7
+  include ruby::1-9-2
+  include ruby::1-9-3
+
+  # common, useful packages
+  package {
+    [
+      'ack',
+      'findutils',
+      'gnu-tar'
+    ]:
+  }
+
+  file { "${boxen::config::srcdir}/our-boxen":
+    ensure => link,
+    target => $boxen::config::repodir
+  }
+}
